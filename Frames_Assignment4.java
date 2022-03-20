780aaf917877077e7c19e4935052161999fd6923
Saravanan099 <99963680+Saravanan099@users.noreply.github.com>
2022-03-20 10:34:29 +0530

    Add files via upload

---
+++ Frames_Assignment4.java
@@ -0,0 +1,33 @@
+package Week4.Day2;
+
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Frames_Assignment4 {
+
+	public static void main(String[] args) {
+		WebDriverManager.chromedriver().setup();
+		ChromeDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+		driver.get("http://leafground.com/pages/frame.html");
+		
+//		find iframe count
+		List<WebElement> FrameCount=driver.findElements(By.tagName("iframe"));
+		 System.out.println("Frame count is: "+FrameCount.size());
+		 
+		 
+		
+		
+		
+}
+	
+}
